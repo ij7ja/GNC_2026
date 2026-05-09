41947b6b9a205c34011b1b01be481270036a1d7f
Shabab <azk75877@gmail.com>
2026-05-09 13:11:38 +0530

    Added some colors

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,59 @@ function divisible(){
 }
 
 
+
+const sqr = (n) => console.log(n*n)
+
+sqr(5)
+const spr = (n) => {
+    console.log("spr");
+    console.log(n*n);
+    console.log(n*n);
+}
+
+
+
+spr(5)
+
+
+const grade = (marks) => {
+    if(marks > 90){
+        return "A"
+    }
+    else if(marks >= 70){
+        return "B"
+    }
+    else{
+        return "Fail"
+    }
+}
+
+console.log(grade(50));
+
+
+
+//CHANGE COLORS ON CLICK
+// let box = document.querySelector(".box");
+// let c = 0;
+
+// box.addEventListener('click', () => {
+//     let colors = ['red', 'blue', 'yellow', 'green', 'gray', 'lime'];
+//     box.style.backgroundColor = colors[c]
+
+//     c = (c+1)%colors.length;
+// })
+
+
+let box = document.querySelector(".box");
+let c = 0;
+
+const change = () => {
+    let colors = ['red', 'blue', 'yellow', 'green', 'gray', 'lime'];
+    box.style.backgroundColor = colors[c]
+    c = (c+1) % colors.length;
+    console.log("Changing");
+    
+}
+
+box.addEventListener('click', change)
+setInterval(change, 1000)
